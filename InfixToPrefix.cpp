8d1961c3bb3c699824ae277a35f4e32ed9bc3594
Nitin Gandhi <24195103+NITIN-ME@users.noreply.github.com>
2018-06-06 02:45:49 +0530

    Infix to Prefix algorithm.

---
+++ InfixToPrefix.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int val(char ch){
+	if(ch == '(' || ch == ')') return -1;
+	if(ch == '*' || ch == '/') return 2;
+	if(ch == '+' || ch == '-') return 1;
+}
+
+string InfixToPrefix(string s){
+	reverse(s.begin(), s.end());
+	for(int i =0 ; i < s.size(); i++){
+		if(s[i] == '(') s[i] = '%';
+	}
+	for(int i =0 ; i < s.size(); i++){
+		if(s[i] == ')') s[i] = '(';
+	}
+	for(int i =0 ; i < s.size(); i++){
+		if(s[i] == '%') s[i] = ')';
+	}
+	cout<<"String is: "<<s<<endl;
+	string f = "";
+	stack<char> st;
+	for(int i = 0 ; i < s.size(); i++){
+		char ch = s[i];
+		//cout<<"CH: "<<ch<<endl;
+		if(ch != '+' && ch != '-' && ch != '*' && ch != '/' && ch != '(' && ch != ')'){
+			f += ch;
+			//cout<<"ADDING1: "<<ch<<endl;
+		}
+		else{
+			if(st.empty() || ch == '('){
+				st.push(ch);
+				//cout<<"PUSHING1: "<<ch<<endl;
+			}
+			else if(ch == ')'){
+				while(st.top() != '('){
+					f += st.top();
+					//cout<<"POPPING1: "<<st.top()<<endl;
+					st.pop();
+				}
+				//cout<<"POPPING2: "<<st.top()<<endl;
+				st.pop();
+			}
+			else{
+				while(val(st.top()) > val(ch) && !st.empty() && ch != '('){
+					f += st.top();
+					//cout<<"ADDING2: "<<st.top()<<endl;
+					//cout<<"POPPING3: "<<st.top()<<endl;
+					st.pop();
+					if(st.size() == 0) break;
+				}
+				st.push(ch);
+				//cout<<"PUSHING2: "<<ch<<endl;
+			}
+		}
+		//cout<<"f: "<<f<<endl;
+	}
+	while(!st.empty()){
+		//cout<<"ADDING3: "<<st.top()<<endl;
+		f += st.top();
+		st.pop();
+	}
+	reverse(f.begin(), f.end());
+	return f;
+}
+
+int main(){
+	cout<<InfixToPrefix("a+(b*c)")<<endl;
+	cout<<InfixToPrefix("a+b*c-d*e")<<endl;
+	cout<<InfixToPrefix("((a+b)*c-d)*e")<<endl;
+	return 0;
+}
